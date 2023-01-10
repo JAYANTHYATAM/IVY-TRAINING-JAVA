2ee17f47ddcc66e757737e8df99822cb220419a0
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-10 18:11:45 +0530

    Second commit

---
+++ src/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args){
+        int nums[] = new int[8];
+        nums[0] = 10;
+        nums[1] = 32;
+        nums[2] = 43;
+        nums[3] = 56;
+        nums[4] = 61;
+        nums[5] = 73;
+        nums[6] = 81;
+        nums[7] = 93;
+
+        for (int i = 0;i<= nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
