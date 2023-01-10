60aafa5c8ecb4c22383652e0beed207d58bb78ed
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-10 18:23:20 +0530

    Second commit

---
+++ src/PrintArrayElements.java
@@ -0,0 +1,8 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int A[] = {44, 76, 42, 5};
+        for (int i = 0;i<=A.length;i++){
+            System.out.println(A[i]);
+        }
+    }
+}
