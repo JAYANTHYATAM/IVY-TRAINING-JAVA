b0df709c4944e1104b001c420d5a89d074a2e2cb
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-10 18:27:15 +0530

    Second commit

---
+++ src/ForEachArray.java
@@ -0,0 +1,8 @@
+public class ForEachArray {
+    public static void main(String[] args){
+        int aRR[] = {1,2,3};
+        for(int i:aRR){
+            System.out.println(i);
+        }
+    }
+}
