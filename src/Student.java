56c41c18c10164b773aaca2cadbdf84b57a525a1
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-11 17:53:38 +0530

    Third commit

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int rollno;
+    Student(){
+        name = "Jayanth";
+        rollno = 8004;
+    }
+public static void main(String[] args){
+        Student s1 = new Student();
+        System.out.println(s1.name);
+        System.out.println(s1.rollno);
+}
+}
