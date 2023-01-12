bb314ba65953cc39fdd156af13d6da091f1c1710
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-12 18:28:45 +0530

    fourth commit

---
+++ src/StaticDemo.java
@@ -0,0 +1,12 @@
+public class StaticDemo {
+    static int a = 10;
+    static void display()
+    {
+        System.out.println("Static Method");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(a);
+        display();
+    }
+}
