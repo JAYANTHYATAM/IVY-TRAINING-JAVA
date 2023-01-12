a5d5edb3d924c3d9a969108046a616ace480b86a
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-12 18:34:04 +0530

    fourth commit

---
+++ src/DemoStatic.java
@@ -0,0 +1,13 @@
+class Demo{
+    static int a = 10;
+    static void display()
+    {
+        System.out.println("Static Method");
+    }
+}
+public class DemoStatic {
+    public static void main(String[] args){
+        System.out.println(Demo.a);
+        Demo.display();
+    }
+}
