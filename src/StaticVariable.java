01b9336dfa53ba6f63d525d3616101322be11480
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-18 18:03:56 +0530

    sixth commit

---
+++ src/StaticVariable.java
@@ -0,0 +1,19 @@
+class Stat {
+    static int a = m1();
+
+    static {
+        System.out.println("Inside Static ");
+    }
+
+    static int m1() {
+        System.out.println("from m1");
+        return 20;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Value of a:" + a);
+        System.out.println("from main");
+    }
+}
+
+
