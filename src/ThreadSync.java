6f6bd19eddbe57d3c68f57d59a9fed205ba77d3e
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-18 15:57:31 +0530

    sixth commit

--- src/ThreadSync.java
+++ src/ThreadSync.java
@@ -1,21 +1,36 @@
-import java.io.*;
 class Example{
     void display(){
-        Thread g =Thread.currentThread();
-        for (int i = 0;i<=5;i++){
-            try{
-                Thread.sleep(1000);
-                System.out.println(g.getName()+""+i);
-            }
-            catch (Exception e){}
+        Thread thread = Thread.currentThread();
+        int[] numbers = {5,20,25,40,2,55} ;
+        int max = numbers[0];
+        int min = numbers[0];
 
+        for (int i = 0; i <=6 ; i++ ) {
+            if (numbers[i] > max)
+                max = numbers[i];
+
+            if (numbers[i] < min)
+                min = numbers[i] ;
         }
+        System.out.println(min);
+        System.out.println(max);
+
+
+            /*try{
+                Thread.sleep(1000);
+                System.out.println(thread.getName() + " number " + i );
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }*/
+
     }
 }
+
 class T extends Thread{
     Example e;
     T (Example e){
-        this .e=e;
+        this.e = e;
     }
     public void run(){
         e.display();
