4a1a86e3779dec1af60f751349cd66bda43c386a
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-19 14:43:13 +0530

    seventh commit

---
+++ src/ConstructorStudent.java
@@ -0,0 +1,9 @@
+public class ConstructorStudent {
+    String name;
+    int rno;
+    public static void main(String args[]){
+        Student s1 = new Student();
+        System.out.println(s1.name);
+        System.out.println(s1.rollno);
+    }
+}
