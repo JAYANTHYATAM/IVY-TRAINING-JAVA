8c9e42472cfbe62ec4d725d1f2f7f7247f434249
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-23 12:57:08 +0530

    seventh commit

---
+++ src/Colleges.java
@@ -0,0 +1,14 @@
+class Colleges{
+    String name;
+     int codeno;
+     Colleges(){
+         name = "JAY";
+         codeno = 8004;
+     }
+
+public static void main(String[] args){
+         Colleges C1 = new Colleges();
+         System.out.println(C1.codeno);
+         System.out.println(C1.name);
+     }
+}
