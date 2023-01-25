f35c43d7b21977416739db841f961383946bb01b
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-25 14:46:11 +0530

    seventh commit

--- src/Arrrays.java
+++ src/Arrrays.java
@@ -1,14 +1,9 @@
 public class Arrrays {
     public static void main(String[] args) {
-
-
-        int arr[] = new int[5];
-        arr[0] = 11;
-        arr[1] = 34;
-        arr[2] = 56;
-        arr[3] = 67;
-        for (int i = 0;i<=arr.length;i++){
-            System.out.println(arr[i]);
+        for (int i = 0 ;i<= 10;i++){
+            System.out.println(i);
         }
+
     }
 }
+// printing first 10 consecutive numbers
