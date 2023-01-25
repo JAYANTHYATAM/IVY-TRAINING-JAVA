a46938e5f6f57956c609812d1eae327486aa9764
jayanth.yatam <jayanthyatam@gmail.com>
2023-01-25 12:50:54 +0530

    seventh commit

---
+++ src/Arrrays.java
@@ -0,0 +1,14 @@
+public class Arrrays {
+    public static void main(String[] args) {
+
+
+        int arr[] = new int[5];
+        arr[0] = 11;
+        arr[1] = 34;
+        arr[2] = 56;
+        arr[3] = 67;
+        for (int i = 0;i<=arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
